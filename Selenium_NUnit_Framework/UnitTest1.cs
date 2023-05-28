219dd1f2b79b7f44de857d35b31fae3e1118966d
harshkd007 <kadamharshavardhan042@gmail.com>
2023-05-28 11:26:11 +0530

    Add project files.

---
+++ Selenium_NUnit_Framework/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Selenium_NUnit_Framework
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
